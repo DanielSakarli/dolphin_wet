766ef5f815ec9cc43d9795573a5f09471d03c9fa
Ziqi He <ziqi.he@fau.de>
2023-05-08 22:16:07 +0200

    26: Set up eslint and prettier for standard code format in the project
    Signed-off-by: Ziqi He <ziqi.he@fau.de>

--- Code/frontend/src/App.vue
+++ Code/frontend/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <ion-app>
-    <ion-router-outlet />
-  </ion-app>
+	<ion-app>
+		<ion-router-outlet />
+		<memories-page />
+	</ion-app>
 </template>
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+console.log('hello');
 </script>
