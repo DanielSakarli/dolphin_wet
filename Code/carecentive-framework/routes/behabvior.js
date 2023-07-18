661600cd6e6fcb45089c3366f08983f78652b8c1
Ziqi He <ziqi.he@fau.de>
2023-07-18 10:37:20 +0200

    feat: finish get request for appropriate behaviour api

--- Code/carecentive-framework/routes/behabvior.js
+++ Code/carecentive-framework/routes/behabvior.js
@@ -4,11 +4,12 @@ const {
 	behaviorPostValidateRequestBody,
 } = require('../validators/dolphinWetValidators');
 const { authenticateTokenWithSwitch } = require('../controllers/authSwitch');
+const { query } = require('express-validator');
 
 /**
  * Controllers
  */
-const { setResult } = require('../controllers/behaviour');
+const { setResult, getTestResult } = require('../controllers/behaviour');
 
 /**
  * Loads test result for good_health
@@ -21,8 +22,12 @@ router.post(
 );
 
 /**
- * Gets test result for good_health
+ * Gets the test result based on given query params.
  */
-router.get('/');
+router.get(
+	'/',
+	[query('name').notEmpty().isString().withMessage('Name can not be empty!')],
+	getTestResult
+);
 
 module.exports = router;
