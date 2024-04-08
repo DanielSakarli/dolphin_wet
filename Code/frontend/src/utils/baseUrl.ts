5942d7cb1b36f614fd53df19f3cdd66f8bde818d
Daniel Sakarli <daniel.sakarli@fau.de>
2024-04-08 14:20:57 +0200

    Random commit

--- Code/frontend/src/utils/baseUrl.ts
+++ Code/frontend/src/utils/baseUrl.ts
@@ -1,1 +1,8 @@
-export const baseUrl = 'http://localhost:3309'; // 'http://88395-17112.pph-server.de'; //
+const NODE_ENV = 'development'; // or 'production'
+
+export const baseUrl =
+	NODE_ENV === 'production'
+		? 'http://88395-17112.pph-server.de'
+		: 'http://localhost:3309';
+// Checks if we are in production or development. If in production it sends http requests
+// to the server, if in development it sends http requests to localhost.
