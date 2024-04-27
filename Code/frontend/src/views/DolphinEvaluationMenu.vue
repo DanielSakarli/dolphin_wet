f054aa82fb4e78ad188558b06a2241a05a1ba9cd
Daniel Sakarli <daniel.sakarli@fau.de>
2024-04-27 13:00:53 +0200

    Dolphin vue file now bug-free

--- Code/frontend/src/views/DolphinEvaluationMenu.vue
+++ Code/frontend/src/views/DolphinEvaluationMenu.vue
@@ -17,7 +17,7 @@
 					@click="selectDolphin(dolphin)"
 				>
 					<ion-label
-						>{{ dolphin.name }} (Born: {{ dolphin.year_of_birth }})</ion-label
+						>{{ dolphin?.name }} (Born: {{ dolphin?.year_of_birth }})</ion-label
 					>
 				</ion-item>
 			</ion-list>
@@ -109,9 +109,7 @@
 								placeholder="Enter maximum kcal calculations"
 							></ion-input>
 						</ion-item>
-						<ion-button expand="full" type="submit" @click="submitEdit">
-							Save Changes
-						</ion-button>
+						<ion-button expand="full" type="submit"> Save Changes </ion-button>
 					</form>
 				</ion-content>
 			</ion-modal>
@@ -219,7 +217,7 @@ import {
 	IonMenuButton,
 	alertController,
 } from '@ionic/vue';
-import { ref, computed, onMounted } from 'vue';
+//import { ref, computed, onMounted } from 'vue';
 import axios from 'axios';
 //import { useRouter } from 'vue-router';
 import { baseUrl } from '@/utils/baseUrl';
@@ -229,7 +227,7 @@ import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 const dolphinsStore = useDolphinsStore();
 
-interface Dolphin {
+/*interface Dolphin {
 	dolphin_id: number;
 	name: string;
 	sex: number;
@@ -243,7 +241,7 @@ interface Dolphin {
 	created_at: string;
 	updated_at: string;
 }
-
+*/
 export default {
 	components: {
 		IonPage,
@@ -271,8 +269,256 @@ export default {
 		changeLanguage($event: any) {
 			this.$i18n.locale = $event.detail.value;
 		},
+
+		// Fetch all dolphins when component is created
+		async fetchDolphins() {
+			try {
+				await axios
+					.get(
+						baseUrl + '/api/dolphins', //'http://88395-17112.pph-server.de/api/dolphins'
+						{ withCredentials: true }
+					)
+					.then((response) => {
+						if (localStorage.getItem('reload_data_button_pressed') === 'true') {
+							toast.success('Dolphin data up to date.', {
+								autoClose: 1500,
+							});
+							localStorage.setItem('reload_data_button_pressed', 'false');
+						}
+						console.log('Response:', response.data);
+						this.dolphins = response.data;
+						this.currentDolphinValues = response.data;
+						console.log('Current dolphin values: ', this.currentDolphinValues);
+					})
+					.catch((e) => {
+						console.error(e);
+
+						toast.error('Error while getting the dolphin data.', {
+							autoClose: 2000,
+						});
+					});
+			} catch (error) {
+				this.errorMessage = 'Error fetching dolphins.';
+			}
+		},
+
+		// Gets called after user added a new dolphin
+		// fetches the newest dolphinList and closes the ion-modal for adding dolphins
+		dolphinAdded() {
+			this.fetchDolphins();
+			this.addModalOpen = false;
+		},
+
+		deleteDolphin() {
+			if (this.currentDolphinValues.name) {
+				this.showAlert().then(() => {
+					let deleteDolphin = this.currentDolphinValues.name;
+					const urlDelete = baseUrl + `/api/dolphins/${deleteDolphin}`;
+					axios
+						.delete(urlDelete, { withCredentials: true })
+						.then((response) => {
+							console.log('Response:', response.data);
+							this.fetchDolphins();
+							this.showDolphin = false;
+							toast.success(`Dolphin ${deleteDolphin} successfully deleted!`, {
+								autoClose: 1700,
+							});
+							deleteDolphin = '';
+						})
+						.catch((error) => {
+							console.error('Error:', error.response.data);
+							if (error.response.data.error === 'USER_NOT_AN_ADMINISTRATOR') {
+								toast.error('User has no administrator rights!', {
+									autoClose: 2000,
+								});
+							}
+							deleteDolphin = '';
+						});
+				});
+			}
+		},
+
+		showAlert() {
+			return new Promise((resolve, reject) => {
+				console.log('Reached the alert message');
+				alertController
+					.create({
+						header: 'Confirmation',
+						message: 'Are you sure you want to delete the dolphin?',
+						buttons: [
+							{
+								text: 'Cancel',
+								role: 'cancel',
+								cssClass: 'secondary',
+								handler: () => {
+									console.log('Cancel clicked');
+									reject();
+								},
+							},
+							{
+								text: 'Delete',
+								handler: () => {
+									console.log('Confirm Okay');
+									resolve(void 0);
+								},
+							},
+						],
+					})
+					.then((alert) => {
+						alert.present();
+					});
+			});
+		},
+
+		// Select a dolphin to view
+		selectDolphin(dolphin: any) {
+			this.currentDolphin = dolphin;
+			/*this.currentDolphinIndex = this.dolphins.findIndex(
+				(d) => d.dolphin_id === dolphin.dolphin_id
+			);*/
+			this.currentDolphinIndex = Object.keys(this.dolphins).indexOf(
+				dolphin.dolphin_id.toString()
+			);
+			this.currentDolphinValues = { ...dolphin };
+			this.showDolphin = true;
+		},
+
+		// Move to the next dolphin
+		nextDolphin() {
+			if (this.nextDolphinAvailable()) {
+				this.currentDolphinIndex += 1; // The '!' after the value asserts that it is not null
+				this.currentDolphin = this.dolphins[this.currentDolphinIndex]; // Here as well
+				this.currentDolphinValues = { ...this.currentDolphin };
+			}
+		},
+
+		nextDolphinAvailable() {
+			return (
+				this.currentDolphinIndex !== null &&
+				this.currentDolphinIndex < Object.keys(this.dolphins).length - 1
+			);
+		},
+		// Move to the previous dolphin
+		prevDolphin() {
+			if (this.prevDolphinAvailable()) {
+				this.currentDolphinIndex -= 1; // And here
+				this.currentDolphin = this.dolphins[this.currentDolphinIndex]; // And here
+				this.currentDolphinValues = { ...this.currentDolphin };
+			}
+		},
+		prevDolphinAvailable() {
+			return this.currentDolphinIndex !== null && this.currentDolphinIndex > 0;
+		},
+		// Open the edit modal
+		openEditModal() {
+			this.editModalOpen = true;
+		},
+		// Close the edit modal
+		closeEditModal() {
+			this.editModalOpen = false;
+		},
+		// Submit edited dolphin values
+		async submitEdit(event: Event) {
+			event.preventDefault();
+
+			if (this.currentDolphinValues) {
+				console.log('Current values: ', this.currentDolphinValues);
+			}
+			const foundDolphin: any = dolphinsStore.dolphinList.find(
+				(dolphin) => dolphin.name === this.currentDolphinValues?.name
+			);
+
+			if (foundDolphin) {
+				//let dolphinCopy = { ...foundDolphin }; // Copies data so I can remove the id later on
+				const dolphinCopy = {
+					name: this.currentDolphinValues?.name,
+					sex: this.currentDolphinValues?.sex,
+					on_site: this.currentDolphinValues?.on_site,
+					year_of_birth: this.currentDolphinValues?.year_of_birth.toString(),
+					place_of_birth: this.currentDolphinValues?.place_of_birth.toString(),
+					min_weight_measured: this.currentDolphinValues?.min_weight_measured,
+					max_weight_measured: this.currentDolphinValues?.max_weight_measured,
+					min_kcal_calculations:
+						this.currentDolphinValues?.min_kcal_calculations,
+					max_kcal_calculations:
+						this.currentDolphinValues?.max_kcal_calculations,
+				};
+
+				/*if (dolphinCopy?.dolphin_id) {
+					delete dolphinCopy.dolphin_id;
+				} //delete dolphin_id since it is not allowed to be changed or be in the request body during a patch request
+				*/
+				console.log('This is sent to backend: ', dolphinCopy);
+				const urlPatch = baseUrl + `/api/dolphins/${dolphinCopy.name}`;
+				await axios
+					.patch(urlPatch, dolphinCopy, { withCredentials: true })
+					.then((response) => {
+						console.log('Response:', response.data);
+						this.closeEditModal();
+						console.log('Current dolphin: ', this.currentDolphin?.name);
+						this.fetchDolphins();
+						toast.success('Dolphin data has been updated.', {
+							autoClose: 1500,
+						});
+						// Set current dolphin name to null and then again to name
+						// Because than the ion-card with the new dolphin data will be updated
+						/*if (currentDolphin.value !== null) {
+						currentDolphin.value.name = null; //closes the ion-card
+					}*/
+						this.showDolphin = false;
+					})
+					.catch((error) => {
+						console.error('Error:', error.response.data);
+					});
+			}
+		},
+		//Adding a new dolphin
+		//const addModalOpen = ref<boolean>(false);
+		//const newDolphin = ref<Dolphin | null>(null);
+
+		reloadDolphins() {
+			localStorage.setItem('reload_data_button_pressed', 'true');
+			this.fetchDolphins();
+		},
+
+		openAddModal() {
+			this.addModalOpen = true;
+			this.newDolphin = {
+				dolphin_id: 0,
+				name: '',
+				sex: 0,
+				on_site: 0,
+				year_of_birth: new Date().getFullYear(),
+				place_of_birth: '',
+				min_weight_measured: 0,
+				max_weight_measured: 0,
+				min_kcal_calculations: 0,
+				max_kcal_calculations: 0,
+				//created_at: '',
+				//updated_at: '',
+			};
+		},
+		//Model and form for adding a dolphin
+		closeAddModal() {
+			this.addModalOpen = false;
+		},
+
+		async submitAdd(event: Event) {
+			event.preventDefault();
+			if (this.newDolphin) {
+				try {
+					await axios.post(baseUrl + '/api/dolphins', this.newDolphin, {
+						withCredentials: true,
+					});
+					this.closeAddModal();
+					this.fetchDolphins();
+				} catch (error) {
+					this.errorMessage = 'Error adding dolphin.';
+				}
+			}
+		},
 	},
-	shouldDisplayKey(key) {
+	shouldDisplayKey(key: string | number) {
 		const keysToDisplay = [
 			'name',
 			'sex',
@@ -284,9 +530,9 @@ export default {
 			'min_kcal_calculations',
 			'max_kcal_calculations',
 		];
-		return keysToDisplay.includes(key);
+		return keysToDisplay.includes(String(key));
 	},
-	getPlaceholder(key) {
+	getPlaceholder(key: string | number) {
 		const placeholders = {
 			name: 'Enter name',
 			sex: 'Enter sex',
@@ -298,55 +544,93 @@ export default {
 			min_kcal_calculations: 'Enter minimum kcal calculations',
 			max_kcal_calculations: 'Enter maximum kcal calculations',
 		};
-		return placeholders[key] || key;
+		return placeholders[key as keyof typeof placeholders] || key;
 	},
 	data() {
 		return {
-			/*currentDolphinValues: {
-				dolphin_id: null,
+			currentDolphinValues: {
+				dolphin_id: -1,
 				name: '',
-				sex: null,
-				on_site: null,
-				year_of_birth: null,
+				sex: -1,
+				on_site: -1,
+				year_of_birth: -1,
 				place_of_birth: '',
-				min_weight_measured: null,
-				max_weight_measured: null,
-				min_kcal_calculations: null,
-				max_kcal_calculations: null,
-			},*/
-			/*currentDolphin: {
-				dolphin_id: null,
+				min_weight_measured: -1,
+				max_weight_measured: -1,
+				min_kcal_calculations: -1,
+				max_kcal_calculations: -1,
+			},
+			dolphins: [
+				{
+					dolphin_id: -1,
+					name: '',
+					sex: -1,
+					on_site: -1,
+					year_of_birth: -1,
+					place_of_birth: '',
+					min_weight_measured: -1,
+					max_weight_measured: -1,
+					min_kcal_calculations: -1,
+					max_kcal_calculations: -1,
+				},
+			],
+			currentDolphin: {
+				dolphin_id: -1,
 				name: '',
-				sex: null,
-				on_site: null,
-				year_of_birth: null,
+				sex: -1,
+				on_site: -1,
+				year_of_birth: -1,
 				place_of_birth: '',
-				min_body_condition_score: null,
-				max_body_condition_score: null,
-				min_kcal_calculations: null,
-				max_kcal_calculations: null,
-			},*/
-			/*dolphins: {
-				dolphin_id: null,
+				min_weight_measured: -1,
+				max_weight_measured: -1,
+				min_kcal_calculations: -1,
+				max_kcal_calculations: -1,
+			},
+			newDolphin: {
+				dolphin_id: -1,
 				name: '',
-				sex: null,
-				on_site: null,
-				year_of_birth: null,
+				sex: -1,
+				on_site: -1,
+				year_of_birth: -1,
 				place_of_birth: '',
-				min_body_condition_score: null,
-				max_body_condition_score: null,
-				min_kcal_calculations: null,
-				max_kcal_calculations: null,
-			},*/
+				min_weight_measured: -1,
+				max_weight_measured: -1,
+				min_kcal_calculations: -1,
+				max_kcal_calculations: -1,
+			},
+			errorMessage: '',
+			addModalOpen: false,
+			showDolphin: false,
+			currentDolphinIndex: 0,
+			editModalOpen: false,
+			isLoading: false,
 		};
 	},
+	async mounted() {
+		this.isLoading = true;
+		this.errorMessage = '';
+		try {
+			const response = await axios.get(
+				baseUrl + '/api/dolphins', //'http://88395-17112.pph-server.de/api/dolphins'
+				{ withCredentials: true }
+			);
+			this.dolphins = response.data;
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				this.errorMessage =
+					error.response?.data.error || 'Unknown error occurred.';
+			}
+		} finally {
+			this.isLoading = false;
+		}
+	},
 	setup() {
-		const dolphins = ref<Dolphin[]>([]);
-		const currentDolphin = ref<Dolphin | null>(null);
-		const currentDolphinIndex = ref<number | null>(null);
+		/*const dolphins = ref<Dolphin[]>([]);
+		//const currentDolphin = ref<Dolphin | null>(null);
+		//const currentDolphinIndex = ref<number | null>(null);
 		const errorMessage = ref<string | null>(null);
-		const editModalOpen = ref<boolean>(false);
-		const currentDolphinValues = ref<Dolphin | null>(null);
+		//const editModalOpen = ref<boolean>(false);
+		//const currentDolphinValues = ref<Dolphin | null>(null);
 		const showDolphin = ref<boolean>(false);
 
 		// For editing the dolphin
@@ -378,6 +662,7 @@ export default {
 		//   router.push({ name: 'AddDolphin' });
 		// };
 
+		
 		// Fetch all dolphins when component is created
 		const fetchDolphins = async () => {
 			try {
@@ -395,8 +680,8 @@ export default {
 						}
 						console.log('Response:', response.data);
 						dolphins.value = response.data;
-						currentDolphinValues.value = response.data;
-						console.log('Current dolphin values: ', currentDolphinValues.value);
+						this.currentDolphinValues = response.data;
+						console.log('Current dolphin values: ', this.currentDolphinValues);
 					})
 					.catch((e) => {
 						console.error(e);
@@ -568,7 +853,7 @@ export default {
 					// Because than the ion-card with the new dolphin data will be updated
 					/*if (currentDolphin.value !== null) {
 						currentDolphin.value.name = null; //closes the ion-card
-					}*/
+					}
 					showDolphin.value = false;
 				})
 				.catch((error) => {
@@ -644,7 +929,7 @@ export default {
 			addModalOpen,
 			newDolphin,
 			submitAdd,
-		};
+		};*/
 	},
 };
 </script>
