e33cd2ccd37a7437c712a12df6c32cdf0b6e0a30
Daniel Sakarli <daniel.sakarli@fau.de>
2024-06-18 10:28:49 +0200

    Changed the floodfill algorithm to start at every non-black non-visited pixel

--- Code/carecentive-framework/controllers/detectMarks.js
+++ Code/carecentive-framework/controllers/detectMarks.js
@@ -12,6 +12,89 @@ async function detectMarks(imagePath) {
 
         const floodFill = (x, y) => {
             const stack = [[x, y]];
+            let pixelCount = 0;
+
+            while (stack.length > 0) {
+                const [currX, currY] = stack.pop();
+                const idx = (currY * width + currX) * 4;
+                if (visited[currY * width + currX]) continue;
+
+                visited[currY * width + currX] = true;
+                const red = image.bitmap.data[idx];
+                const green = image.bitmap.data[idx + 1];
+                const blue = image.bitmap.data[idx + 2];
+
+                if (isBlack(red, green, blue)) continue;
+
+                if (isWhiteOrRed(red, green, blue)) {
+                    pixelCount++;
+                }
+
+                if (currX > 0 && !visited[currY * width + (currX - 1)]) stack.push([currX - 1, currY]);
+                if (currX < width - 1 && !visited[currY * width + (currX + 1)]) stack.push([currX + 1, currY]);
+                if (currY > 0 && !visited[(currY - 1) * width + currX]) stack.push([currX, currY - 1]);
+                if (currY < height - 1 && !visited[(currY + 1) * width + currX]) stack.push([currX, currY + 1]);
+            }
+
+            return pixelCount;
+        };
+
+        const areas = [];
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                const idx = (y * width + x) * 4;
+                const red = image.bitmap.data[idx];
+                const green = image.bitmap.data[idx + 1];
+                const blue = image.bitmap.data[idx + 2];
+
+                if (!isBlack(red, green, blue) && !visited[y * width + x]) {
+                    const areaSize = floodFill(x, y);
+                    if (areaSize > 0) {
+                        areas.push(areaSize);
+                    }
+                }
+            }
+        }
+
+        console.log("Detected areas (number of pixels in each floodfill):", areas);
+        return areas;
+    } catch (error) {
+        console.error('Error processing image:', error);
+        throw error;
+    }
+}
+
+module.exports = { detectMarks };
+
+// Run the function (uncomment the lines below if you want to test it)
+// detectMarks('/mnt/data/test.png')
+//     .then(result => console.log(result))
+//     .catch(error => console.error(error));
+
+
+
+
+
+
+
+
+
+
+
+/*const Jimp = require('jimp');
+
+async function detectMarks(imagePath) {
+    try {
+        const image = await Jimp.read(imagePath);
+        const { width, height } = image.bitmap;
+        const visited = new Array(width * height).fill(false);
+
+        const isBlack = (red, green, blue) => red < 50 && green < 50 && blue < 50;
+        const isRed = (red, green, blue) => red > 200 && green < 100 && blue < 100;
+        const isWhiteOrRed = (red, green, blue) => (red > 200 && green < 100 && blue < 100) || (red > 200 && green > 200 && blue > 200);
+
+        const floodFill = (x, y) => {
+            const stack = [[x, y]];
             let redPixelCount = 0;
             let silhouettePixelCount = 0;
 
@@ -72,7 +155,7 @@ async function detectMarks(imagePath) {
                         if (isWhiteOrRed(startRed, startGreen, startBlue) && !visited[y * width + (x - 1)]) {
                             silhouettes.push(floodFill(x - 1, y));
                         }*/
-                    } else if (y > 0) {
+                   /* } else if (y > 0) {
                         const startIdx = ((y - 1) * width + x) * 4;
                         const startRed = image.bitmap.data[startIdx];
                         const startGreen = image.bitmap.data[startIdx + 1];
@@ -95,7 +178,7 @@ async function detectMarks(imagePath) {
 }
 
 module.exports = { detectMarks };
-
+*/
 
 /*const Jimp = require('jimp');
 
